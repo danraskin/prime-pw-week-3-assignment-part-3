13753b3aafb175a7ea4a4f66678e7db6bf60f85d
Dan Raskin <Danieltraskin@gmail.com>
2022-07-29 11:21:42 -0500

    completed problems 1-7. noted some questions/challenges I will look up before submitting assignment.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,20 +4,28 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+let partsNeeded = 40;
+console.log(partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -25,12 +33,32 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let x=0; x<supplyChanges.length;x++) {
+    if (supplyChanges[x]>0) {
+            console.log(`added ${supplyChanges[x]} parts.`);
+        } else if (supplyChanges[x]===0) {    //something very interesting is going on here with the "===" operator.
+                                            //when supplyChanges[x]=0, and the for loop seems to *change* the value of supplyChanges[2] and [3] to 0, and then skips the output (doesn't log)
+                                            //I am getting to this assignment after class/office hours this week, but I am curious why this is! Will investigate.
+            console.log('No change.');
+            console.log(supplyChanges)
+        } else if (supplyChanges[x]<0) {
+            console.log(`Removed ${supplyChanges[x]} parts.`);
+        }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (let x of supplyChanges) { 
+    if (x>0) { //i got stuck on the identify of the x var in the for of loop; must look at an explanation, but it seems that x is set to the value of array index, and 'for of' carries out the index looping in the background without an identified variable?
+        console.log(`added ${x} parts.`);
+    } else if (x===0) {
+        console.log('No change.');
+    } else if (x<0) {
+        console.log(`Removed ${x} parts.`);
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
